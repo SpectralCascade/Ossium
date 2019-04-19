948b7709830de6e000f5ef25b0aabfb37d254747
SpectralCascade <32915090+SpectralCascade@users.noreply.github.com>
2019-04-19 21:40:51 +0100

    Added picker function for picking items from vectors.

---
+++ src/Ossium/randutils.h
@@ -0,0 +1,26 @@
+#ifndef RANDUTILS_H
+#define RANDUTILS_H
+
+#include <functional>
+
+using namespace std;
+
+namespace Ossium
+{
+
+    template<class T>
+    T* Pick(vector<T> data, function<bool(T&)> picker)
+    {
+        for (auto item : data)
+        {
+            if (picker(item))
+            {
+                return &item;
+            }
+        }
+        return nullptr;
+    }
+
+}
+
+#endif // RANDUTILS_H
