b07a836692ee414329ca3b1851c6918d62bfa639
Tim Lane <timlanesoftware@gmail.com>
2020-04-17 22:58:11 +0100

    Added basic button components

---
+++ Engine/src/Components/button.cpp
@@ -0,0 +1,60 @@
+#include "button.h"
+
+using namespace Ossium;
+using namespace Ossium::Input;
+
+inline namespace UI
+{
+
+    REGISTER_COMPONENT(Button);
+
+    void Button::OnCreate()
+    {
+        GraphicComponent::OnCreate();
+        /// We can't inherit from StateSprite as we already inherit indirectly from GraphicComponent,
+        /// but we can add our own instance to the ECS.
+        sprite = entity->AddComponentOnce<StateSprite>();
+    }
+
+    void Button::Render(Renderer& renderer)
+    {
+    }
+
+    bool Button::ContainsPointer(Point position)
+    {
+        return sprite->GetRect(GetTransform()->GetWorldPosition()).Contains(position);
+    }
+
+    void Button::OnClick()
+    {
+        sprite->ChangeSubState(1);
+        OnClicked(*this);
+    }
+
+    void Button::OnPointerDown()
+    {
+        sprite->ChangeSubState(2);
+    }
+
+    void Button::OnPointerUp()
+    {
+        sprite->ChangeSubState(0);
+    }
+
+    void Button::OnHoverBegin()
+    {
+        if (!IsPressed())
+        {
+            sprite->ChangeSubState(1);
+        }
+    }
+
+    void Button::OnHoverEnd()
+    {
+        if (!IsPressed())
+        {
+            sprite->ChangeSubState(0);
+        }
+    }
+
+}
---
+++ Engine/src/Components/button.h
@@ -0,0 +1,44 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "../Ossium.h"
+
+using namespace Ossium;
+using namespace Ossium::Input;
+using namespace Ossium::Graphics;
+
+inline namespace UI
+{
+
+    class Button : public InteractableGUI
+    {
+    public:
+        DECLARE_COMPONENT(InteractableGUI, Button);
+
+        /// Adds a sprite component and sets up a reference to it.
+        void OnCreate();
+
+        /// Does nothing because we have a separate sprite component for the actual graphics.
+        void Render(Renderer& renderer);
+
+        /// Override to use sprite rect.
+        bool ContainsPointer(Point position);
+
+        /// Callback registry that is called whenever the button is clicked
+        Callback<Button> OnClicked;
+
+        /// Reference to the sprite component this button requires.
+        StateSprite* sprite = nullptr;
+
+    protected:
+        void OnClick();
+        void OnHoverBegin();
+        void OnHoverEnd();
+        void OnPointerDown();
+        void OnPointerUp();
+
+    };
+
+}
+
+#endif // BUTTON_H
