85881c2051fbb580cdc530dc6d344b895c5274c6
Tim Lane <timlanesoftware@gmail.com>
2022-09-13 14:17:39 +0100

    Further project structure cleanup (Android builds not yet supported via soupbuild)

---
+++ Engine/src/mainAndroid.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := main
+
+SDL_PATH := ../SDL
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/../SDL2_image/ $(LOCAL_PATH)/../SDL2_ttf/ $(LOCAL_PATH)/../SDL2_mixer/ $(LOCAL_PATH)/../SDL2_net/ $(LOCAL_PATH)/../Box2D
+
+# Add your application source files here...
+LOCAL_SRC_FILES := Editor/main.cpp Editor/Core/editorserializer.cpp $(subst $(LOCAL_PATH)/,, \
+  	$(wildcard $(LOCAL_PATH)/Core/*.cpp) \
+  	$(wildcard $(LOCAL_PATH)/Components/*.cpp) \
+  	$(wildcard $(LOCAL_PATH)/Components/UI/*.cpp))
+
+# Box2D and other static libs
+LOCAL_STATIC_LIBRARIES := Box2D
+
+# SDL2 and extension libraries
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_ttf SDL2_mixer SDL2_net
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
+
+include $(BUILD_SHARED_LIBRARY)
